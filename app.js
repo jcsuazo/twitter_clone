270adc66e5776716daef68ae5fc5e8fafe5715de
jcsuazo <jsuazo9@gmail.com>
2020-12-27 17:05:37 -0400

    added home page

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const port = 3003;
+
+const server = app.listen(port, () =>
+  console.log('Server listening on por ' + port),
+);
+
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
+app.get('/', (req, res, next) => {
+  let payload = {
+    pageTitle: 'Home',
+  };
+  res.status(200).render('home', payload);
+});
